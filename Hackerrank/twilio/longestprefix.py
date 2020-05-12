31b17553d04d9c15687a8b1f4319b34e06969902
Daniel <danstad2012@gmail.com>
2020-05-11 22:52:42 -0700

    convert children array to object

--- Hackerrank/twilio/longestprefix.py
+++ Hackerrank/twilio/longestprefix.py
@@ -38,56 +38,76 @@ def fancyMatch(prefixes, numbers):
   longestPre = ['']*len(numbers)
 
   #add each prefix into tree
-  d = DeciTree()
+  d = DeciNode()
   for p in prefixes:
-    d.head.addChild(p)
+    print(p)
+    d.addChild(p[1:])
 
   for i,n in enumerate(numbers):
     longestPre[i] = d.getLongestPrefix(n)
+    print('\n')
 
   return longestPre
 
 
 
-class DeciNode:
-  def __init__(self, value):
-    self.value = value
-    self.children = [None] * 10
-    self.tail = False
-
   #sort values by numeral value
     #example: 48639 = (head)-->(4)-->(8)-->(6)-->(3)-->(9)--> None
     #                             \
     #         432                  ->(3)-->(2)-->None
+class DeciNode:
+  def __init__(self, value='+'):
+    self.value = value
+    self.children = {}
+    self.tail = False
+
   def addChild(self, value):
-    if len(value):
-      cut = value[1:]
-      val = int(value[0])
 
-      if self.children[val] == None:
-        self.children[val] = DeciNode(val)
+    if value[0] == '+':
+      value = value[1:]
 
-      self.children[val].addChild(cut)
+    idx = value[0]
+    if len(value) > 2:
+      try:
+        self.children[idx].addChild(value[1:])
+      except:
+        self.children[idx] = DeciNode(idx)
+        self.children[idx].addChild(value[1:])
 
-    else:
-      self.tail = True
+    elif len(value) == 1:
+      if not self.children[idx]:
+        self.children[idx] = DeciNode(idx)
 
-class DeciTree:
-  def __init__(self,value = None):
-    self.head = DeciNode('Head')
+      self.children[idx].tail = True
+
+    # if len(value):
+    #   cut = value[1:]
+    #   val = int(value[0])
+
+    #   if self.children[val] == None:
+    #     self.children[val] = DeciNode(val)
+
+    #   self.children[val].addChild(cut)
+
+    # else:
+    #   self.tail = True
 
   def getLongestPrefix(self, number='', i=0, path='', lg2=''):
+    print(number,i,path,lg2)
+
     if i < len(number):
       nth = number[i]
-
+      print(nth)
       if self.tail:
         lg2 = path
 
-      if  self.children[nth]:
-        self.children[nth].getLongestPrefix(i+1,path+self.value,lg2)
+      if self.children[nth]:
+        self.children[nth].getLongestPrefix(number,i+1,path+str(self.value),lg2)
 
     return(lg2)
-    # number is going through tree by matching number[x] and xth branch is true
+
+
+    # number is going through tree by matching number[x] and xth branch if true
         ###try adding a try except block, and compare function times
           # try:
           #   path.append(something)
@@ -95,8 +115,8 @@ class DeciTree:
           # except:
           #   if self.value == None:
 
-pre = ['42','35','423']
-phonums = ['426568','456789','4239999']
+pre = ['+426','+35','+423']
+phonums = ['+426568','+456789','+4239999']
 
 # print(match(pre, phonums))
 print(fancyMatch(pre, phonums))
