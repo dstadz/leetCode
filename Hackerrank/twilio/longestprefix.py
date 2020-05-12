9851e71c13f555f1fa8b55ec58ba58902251a7e2
Daniel <danstad2012@gmail.com>
2020-05-12 16:03:27 -0700

    testing with larger, random arrays

--- Hackerrank/twilio/longestprefix.py
+++ Hackerrank/twilio/longestprefix.py
@@ -4,6 +4,7 @@ import os
 import random
 import re
 import sys
+import time
 
 
 #
@@ -65,41 +66,57 @@ class DeciNode:
 
     idx = value[0]
 
-    if idx not in self.children:
-      # print(f'{self.value}: {idx} joining {self.children.keys()}')
+    if idx not in self.children: # print(f'{self.value}: {idx} joining {self.children.keys()}')
       self.children[idx] = DeciNode(idx)
 
-    if len(value) >= 2:
-      # print(f'{self.value}:{idx} {value[1:]}=> {self.children.keys()} ')
+    if len(value) >= 2: # print(f'{self.value}:{idx} {value[1:]}=> {self.children.keys()} ')
       self.children[idx].addChild(value[1:])
     else:
       self.children[idx].tail = True
 
-    
+
     # print(idx, value[1:], self.children)
 
 
 
-  def getLongestPrefix(self, number, i=0, path='', lg2=''):
-    # print(number,i,path,lg2)
-    # print(f'value: {self.value}, kids:{self.children}')
-    if number[0] == '+':
-      number = number[1:]
+  def getLongestPrefix(self, number):
+    path = self.value
+    i = 0
+    lg2=''
+    while i < len(number)-1:
 
-    if i < len(number):
-      nth = number[i]
-      # print(nth)
       if self.tail:
+        print(path, number)
         lg2 = path
 
-      if self.children[nth]:
-        print(number,i+1,path+str(self.value),lg2)
-        self.children[nth].getLongestPrefix(number,i+1,path+str(self.value),lg2)
 
-    return(lg2)
+      if number[i] in self.children:
+        self = self.children[number[i]]
+        path += self.value
 
-pre = ['+4265','+351','+423','+5786','2584']
-phonums = ['+426568','+456789','+4239999']
+      i += 1
+      
+    return lg2
 
-# print(match(pre, phonums))
+
+l = 10
+
+pre = ['+4265','+351','+42','+5786','+2584', '+45']
+# phonums = ['+426568','+456789','+4239999']
+phonums = [''] * l
+
+
+for i,n in enumerate(range(l)):
+  x = math.floor(random.random()*10**9)
+  phonums[i] = '+' + str(x)
+print(pre)
+print(phonums, '\n')
+'''
+start = time.time()
+print("hello")
+end = time.time()
+print(end - start)
+'''
+
+print(match(pre, phonums))
 print(fancyMatch(pre, phonums))
