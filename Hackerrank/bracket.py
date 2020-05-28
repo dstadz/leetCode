ff1610b9b3a6245e95ac7d73157191473d5c3f3a
Daniel <danstad2012@gmail.com>
2020-05-28 08:48:12 -0700

    bracket

---
+++ Hackerrank/bracket.py
@@ -0,0 +1,25 @@
+def balancedBrackets(string):
+    # Write your code here
+  stack = []
+  for s in string:
+    print(stack,s)
+    if s == '(' or s=='{' or s=='[' or s=='|':
+      stack.append(s)
+    if len(stack) == 0:
+      return False
+    if len(stack) > 1 and s=='|' and stack[-1]=='|':
+      stack.pop()
+    if s==')' and stack[-1]=='(':
+      stack.pop()
+    if s==']' and stack[-1]=='[':
+      stack.pop()
+    if s=='}' and stack[-1]=='{':
+      stack.pop()
+
+  return len(stack) == 0:
+ 
+  return stack
+
+loops = '{()|}'
+
+print(balancedBrackets(loops))
