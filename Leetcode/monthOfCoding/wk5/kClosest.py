5199fa1d136afc9854123217cd45121ba6261016
Daniel <danstad2012@gmail.com>
2020-05-31 23:04:42 -0700

    add bin tree

--- Leetcode/monthOfCoding/wk5/kClosest.py
+++ Leetcode/monthOfCoding/wk5/kClosest.py
@@ -1,8 +1,36 @@
 import math
+
+class BinarySearchTree:
+    def __init__(self, value = None, coor = None):
+        self.value = value
+        self.coor = coor
+        self.left = None
+        self.right = None
+        
+    def insert(self, value):
+        if self.value == None:
+            self.value = value
+            return
+        elif self.value <= value:
+            if self.right:
+                return self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+        else:
+            if self.left:
+                return self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
         
         closest = [None] * K
+        tree = BinarySearchTree()
         
         for p in points:
             d = math.sqrt(p[0] ** 2 + p[1] ** 2)
+            tree.insert(d,p)
+        
+        
+            
