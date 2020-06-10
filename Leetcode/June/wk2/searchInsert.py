a26f19dddec7f955254509a1bae8b05b6926919c
Daniel <danstad2012@gmail.com>
2020-06-10 10:03:53 -0700

    search insert

---
+++ Leetcode/June/wk2/searchInsert.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+        if len(nums) == 0:
+            return 0        
+        
+        if len(nums) == 1 & nums[0] == target:
+            return 0
+        
+        if target <= nums[0]:
+            return 0
+        if nums[-1] == target:
+            return len(nums) - 1
+        if nums[-1] < target:
+            return len(nums)
+        if len(nums) == 2:
+            return 1
+        
+        left = 0
+        right = len(nums) - 1
+        while left + 1 < right:
+            idx = (left + right) // 2
+            if target <= nums[idx]:
+                right = idx
+
+            elif nums[idx] <= target:
+                left = idx
+        
+        return left + 1
