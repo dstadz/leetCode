00b58850884ffeda5fe7358741b84fa6facfcf5c
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-08-23 10:17:18 -0700

    max-inc-skyline done

--- Leetcode/august/max-inc-skyline.js
+++ Leetcode/august/max-inc-skyline.js
@@ -1,23 +1,27 @@
 var maxIncreaseKeepingSkyline = function(grid) {
-  console.log(grid)
 
   if (grid.length < 2) return 0
   if (grid[0].length < 2) return 0
 
   let floors = 0
-  const shortestShadow = (y,x) => {
-  
-    let vert = min(grid[x])
+  let highRow = []
+  let highCol = []
 
-    let horzCol = []
-    for (let i = 0; i < grid[y].length; i++) {
-      hortzcol.append(grid[y][i])
+  for (let y = 0; y < grid.length; y++){
+    let col = []
+    for (let x = 0; x < grid[y].length; x++){
+      col.push(grid[x][y])
     }
-    let horz = min(horzCol)
-    return min(vert,horz) //shortest shadow
+
+    highCol.push(Math.max(...col))
+    highRow.push(Math.max(...grid[y]))
   }
 
-
+  for (let y = 0; y < grid.length; y++){
+    for (let x = 0; x < grid[y].length; x++){
+      floors += Math.min(highCol[y],highRow[x]) - grid[x][y]
+    }
+  }
 
   return floors
 };
@@ -26,3 +30,5 @@ var maxIncreaseKeepingSkyline = function(grid) {
 
 const skyline =  [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
 console.log(maxIncreaseKeepingSkyline(skyline))
+// Runtime: 116 ms, faster than 19.93% of JavaScript online submissions for Max Increase to Keep City Skyline.
+// Memory Usage: 38.4 MB, less than 31.56% of JavaScript online submissions for Max Increase to Keep City Skyline.
